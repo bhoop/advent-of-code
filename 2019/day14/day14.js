7679ccd07fef218fd51432f19464ea9ae0cb1fc4
Briley Hooper <briley@gmail.com>
2019-12-21 21:43:56 -0500

    day 14 part 2

--- 2019/day14/day14.js
+++ 2019/day14/day14.js
@@ -1,7 +1,7 @@
 
 
 module.exports = function(input) {
-  let ingredients = {}, stockpile = { ORE:0 }, produced = { ORE:0 };
+  let ingredients = {}, stockpile = { ORE:1000000000000 }, produced = { ORE:0 };
 
   // parse ingredients into a data structure we can read from
   input.forEach(formula => {
@@ -16,9 +16,10 @@ module.exports = function(input) {
 
   const make = (ingredient) => {
     if (ingredient === 'ORE') {
-      stockpile.ORE++;
-      produced.ORE++;
-      return;
+      throw 'out-of-stock';
+      // stockpile.ORE++;
+      // produced.ORE++;
+      // return;
     }
 
     const { qty, components } = ingredients[ ingredient ];
@@ -32,10 +33,40 @@ module.exports = function(input) {
     stockpile[ ingredient ] += qty;
   }
 
-  // now that we have the ingredients, start from
-  make('FUEL');
+  const calc = (id) => {
+    if (id === 'ORE') return 1;
+    const o = ingredients[ id ].qty;
+    return ingredients[ id ].components.reduce((p,v) => {
+      return p + v.qty / o * calc( v.id );
+    }, 0);
+  }
 
-  console.log(`1 FUEL TAKES ${produced.ORE} ORE`);
+  const orePerFuel = calc('FUEL');
+  console.log(`1 FUEL TAKES ${calc('FUEL')} ORE`);
+  const fuelPerTrillionOre = Math.floor(1000000000000 / orePerFuel);
+  console.log(`1 TRILLION ORE PRODUCES ${fuelPerTrillionOre} FUEL`);
+
+  // now that we have the ingredients, start from
+  // make('FUEL');
+
+  // console.log(1000000000000 - stockpile.ORE);
+
+  // console.log(Object.keys(produced).map( id => {
+  //   return `${id} PRODUCED=${produced[id]} LEFTOVER=${stockpile[id]}`;
+  // }));
+  // while (true) {
+  //   try {
+  //     make('FUEL');
+  //     console.log(stockpile.ORE);
+  //   } catch (err) {
+  //     if (err !== 'out-of-stock') throw err;
+  //     break;
+  //   }
+  // }
+
+  // console.log(`1 FUEL TAKES ${produced.ORE} ORE (${stockpile.ORE} LEFTOVER)`);
+
+  // console.log(`YOU CAN MAKE ${ Math.floor(1000000000000 / (produced.ORE - stockpile.ORE)) } FUEL WITH 1 TRILLION ORE`);
 }
 
 function parseIngredient(str) {
