a534c2c70a5c134ad78228fbdd9a8526cadc193f
Briley Hooper <briley@gmail.com>
2019-12-21 19:28:31 -0500

    day 13 part 2

--- 2019/day13/day13.js
+++ 2019/day13/day13.js
@@ -5,26 +5,39 @@ const Display = require("../display");
 module.exports = function([input]) {
   let program = parse(input);
 
+  // free credits!
+  program[0] = 2;
+
   let board = new Display();
+  let score = 0;
+  let ballX = -1;
+  let paddleX = -1;
 
   let state = { program, i:0, rbase:0, inputs:[], outputs:[] };
   while (state.i !== false) {
     state = produce(state, next => {
+      next.inputs = [ballX === paddleX ? 0 : (ballX < paddleX ? -1 : 1)];
       exec(next);
 
       if (next.outputs.length === 3) {
-        board.set.apply(board, next.outputs);
+        const [x, y, t] = next.outputs;
+        if (x === -1 && y === 0) score = t;
+        else {
+          if (t === 3) paddleX = x;
+          if (t === 4) ballX = x;
+          board.set(x, y, t);
+        }
         next.outputs = [];
       }
     });
   }
 
-  console.log('Board has', Object.values(board.values).filter(v => v === 2).length, 'blocks');
-  // board.render({
-  //   0: ' ', // empty
-  //   1: '█', // wall
-  //   2: '■', // block
-  //   3: '━', // paddle
-  //   4: '⚾', // ball
-  // });
+  console.log('Score is', score);
+  board.render({
+    0: ' ', // empty
+    1: '█', // wall
+    2: '■', // block
+    3: '━', // paddle
+    4: '⚾', // ball
+  });
 }
